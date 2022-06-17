895792556b27c9015c9b1593384cf47237338662
hmalik129 <hmalik129@gmail.com>
2022-06-17 16:42:45 -0400

    Inserted Action Category Counts
    Retrieving data through Created lifecycle hook rather than Mounted

--- components/widgets/action-agenda/aa-cats/index.vue
+++ components/widgets/action-agenda/aa-cats/index.vue
@@ -5,14 +5,14 @@
     <div class="view-filtered-items">
       <div class="container-fluid">
         <transition-group name="flip-list" tag="div" class="row">
-          <div v-for="({ image, url, identifier,name, count },index) in cats" v-bind:key="identifier"
-            :class="{'col-md-3': index < 8, 'col-md-4': index >= 8 }" class="cont  p-0">
+          <div v-for="({ image, url, identifier, name, count }, index) in cats" v-bind:key="identifier" :class="{ 'col-md-3': index < 8, 'col-md-4': index >= 8 }" class="cont  p-0">
             <a :href="url">
               <img class="img-fluid" :src="image[0]" />
               <div class="overlay">
-                <h3 class="absolute-top" style="color: white; height: 50%;">{{name}}</h3>
-                <div class="badge">
-                  <h3 class="absolute-center" style="color: white; height: 60%;">{{count}}</h3>
+                <h3 class="absolute-center" style="color: white; height: 50%;">{{ name }}</h3>
+                <div class="background-circle" style="color: white;">
+                  {{ count }}
+                  <!-- <h3 class="absolute-center" style="color: white; height: 60%;">{{count}}</h3> -->
                 </div>
               </div>
             </a>
@@ -23,168 +23,155 @@
   </div>
 </template>
 <script>
-  import {
-    getData
-  } from '@action-agenda/cached-apis'
+import { getData } from "@action-agenda/cached-apis";
+import UpdatesWidgetHeader from "./header";
+import axios from "axios";
 
-  import UpdatesWidgetHeader from './header'
-  import axios from 'axios'
+export default {
+  name: "AACatsWidget",
+  components: { UpdatesWidgetHeader },
+  methods: { getCategoryCount },
+  created,
+  mounted,
+  data,
+};
 
-  export default {
-    name: 'AACatsWidget',
-    components: {
-      UpdatesWidgetHeader
-    },
-    methods: {
-      getCategoryCount
-    },
-    mounted,
-    data
+function data() {
+  return {
+    cats: [],
+  };
+}
+
+async function created() {
+  this.cats = (await getData("actionCategories"))
+    .map((cat) => {
+      cat.image = [`https://attachments.cbd.int/${cat.identifier}.jpg`, cat.image];
+      return cat;
+    })
+    .sort(() => 0.5 - Math.random());
+
+  setTimeout(() => {
+    this.cats = this.cats.sort(compare);
+  }, 1000);
+
+  // this.cats.map(async (cat) => {
+  //   cat.count = await getCategoryCount(cat.identifier)
+  //   return cat
+  // })
+
+  /* TEMPORARY SOLUTION: */
+  for (const cat of this.cats) {
+    cat.count = await getCategoryCount(cat.identifier);
+  }
+}
+
+async function mounted() {
+  // this.cats = (await getData('actionCategories'))
+  //   this.cats.map((cat) => {
+  //     cat.image = [`https://attachments.cbd.int/${cat.identifier}.jpg`, cat.image]
+  //     return cat
+  //   })
+  //   .sort(() => 0.5 - Math.random())
+  // setTimeout(() => {
+  //   this.cats = this.cats.sort(compare);
+  // }, 1000)
+}
+
+function compare(a, b) {
+  if (a.identifier < b.identifier) return -1;
+  if (a.identifier > b.identifier) return 1;
+
+  return 0;
+}
+
+async function getCategoryCount(cat) {
+  const q = { "actionDetails.actionCategories": { identifier: cat } };
+  const { count } = (await axios.get("https://api.cbd.int/api/v2019/actions", { params: { q, c: 1 } })).data;
+
+  console.log(cat + ":: " + count);
+
+  if (count === "undefined") {
+    console.log("It is undefined -->" + count);
+    return 1;
   }
 
-  function data() {
-    return {
-      cats: []
-    }
-  }
-
-  async function mounted() {
-
-    this.cats = (await getData('actionCategories'))
-      .map((cat) => {
-        cat.image = [`https://attachments.cbd.int/${cat.identifier}.jpg`, cat.image]
-        return cat
-      })
-      .sort(() => 0.5 - Math.random())
-
-    setTimeout(() => {
-      this.cats = this.cats.sort(compare);
-    }, 1000)
-
-    for (const cat of this.cats) {
-      cat.count = await getCategoryCount(cat.identifier)
-    }
-
-    // this.cats = this.cats.map(async (cat) =>{
-    //    cat.number = await getCategoryCount(cat.identifier)
-    //    return cat
-    // })
-
-    console.log("Cats Length: " + this.cats.length)
-  }
-
-  function compare(a, b) {
-    if (a.identifier < b.identifier)
-      return -1;
-    if (a.identifier > b.identifier)
-      return 1;
-
-    return 0;
-  }
-
-  async function getCategoryCount(cat) {
-
-    const q = {
-      'actionDetails.actionCategories': {
-        "identifier": cat
-      }
-    }
-    const {
-      count
-    } = (await axios.get('https://api.cbd.int/api/v2019/actions', {
-      params: {
-        q,
-        c: 1
-      }
-    })).data
-
-    console.log(cat + ":: " + count)
-
-    if (count === 'undefined') {
-      console.log("It is undefined -->" + count)
-      return 1
-    }
-
-    return count
-  }
+  return count;
+}
 </script>
 <style scoped>
+img {
+  width: 100%;
+}
+
+.flip-list-move {
+  transition: transform 1s;
+}
+
+.overlay {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: 100%;
+  width: 100%;
+  opacity: 0;
+  transition: 0.5s ease;
+  background-color: #000;
+
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+}
+
+.background-circle {
+  height: 15vh;
+  width: 5vw;
+  text-align: center;
+  font-size: 2rem;
+  vertical-align: middle;
+  border-radius: 50%;
+  background: rgb(88, 88, 88);
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 3vh;
+}
+
+.absolute-center {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+}
+
+.cont:hover .overlay {
+  opacity: 0.9;
+}
+
+@media (min-width: 768px) {
   img {
-    width: 100%;
+    max-height: 120px;
   }
+}
 
-  .flip-list-move {
-    transition: transform 1s;
+@media (min-width: 992px) {
+  img {
+    max-height: 155px;
   }
+}
 
-  .overlay {
-    position: absolute;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    height: 100%;
-    width: 100%;
-    opacity: 0;
-    transition: .5s ease;
-    background-color: #000;
-
-    display: flex;
-    justify-content: center;
-    flex-direction: column;
-    align-items: center;
+@media (min-width: 1200px) {
+  img {
+    max-height: 155px;
   }
+}
 
-  .badge {
-    height: 100px;
-    width: 100px;
-    display: table-cell;
-    text-align: center;
-    vertical-align: middle;
-    border-radius: 50%;
-    /* may require vendor prefixes */
-    background: rgb(88, 88, 88);
+@media (min-width: 1600px) {
+  img {
+    max-height: 230px;
   }
-
-  .absolute-top {
-    /* display: flex; */
-    /* align-items:center; */
-    justify-content: center;
-    text-align: center;
-  }
-
-  .absolute-center {
-    /* display: flex; */
-    align-items: center;
-    justify-content: center;
-    text-align: center;
-  }
-
-  .cont:hover .overlay {
-    opacity: .9;
-  }
-
-  @media (min-width: 768px) {
-    img {
-      max-height: 120px;
-    }
-  }
-
-  @media (min-width: 992px) {
-    img {
-      max-height: 155px;
-    }
-  }
-
-  @media (min-width: 1200px) {
-    img {
-      max-height: 155px;
-    }
-  }
-
-  @media (min-width: 1600px) {
-    img {
-      max-height: 230px;
-    }
-  }
+}
 </style>
