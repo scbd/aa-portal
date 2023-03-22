22b6d8d394d316b5a4de69fac0a9a4f401d941e2
Randy Houlahan <randy.houlahan@cbd.int>
2023-03-22 11:24:58 -0400

    text change

--- components/page/HeroHeader/action-agenda-hero.vue
+++ components/page/HeroHeader/action-agenda-hero.vue
@@ -4,11 +4,11 @@
       <div class="col-md-6 left px-0" style="min-height: 450px;">
         <div class="absolute-center">
           <div class="box">
-            <h1 style="color:#009b48">{{ $t("COP15: Join the Action Agenda") }}</h1>
+            <h1 style="color:#009b48">{{ $t("Join the Action Agenda") }}</h1>
             <div>
-              {{ $t("Reversing biodiversity loss requires action from all segments of society!") }}
 
-              {{ $t("It’s in everyone’s best interest to take action and put biodiversity on a path to recovery by 2030! Take leadership and inspire others to safeguard, restore and invest in biodiversity.") }}
+
+              {{ $t("With the adoption of the Kunming-Montreal Global Biodiversity Framework, it’s in everyone’s best interest to take action and put biodiversity on a path to recovery by 2030!") }}
             </div>
             <div>
               <a href="https://www.cbd.int/action-agenda/contribute" class="summit-button mt-4 button js-form-submit form-submit btn btn-primary form-control but-size pt-2">
